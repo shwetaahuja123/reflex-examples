cf58d275842ecf2b0d3fea3202b86d74d31d1460
shwetaahuja123 <shwetaahuja.dce@gmail.com>
2019-08-04 13:56:15 +0530

    delete task

--- frontend/src/Frontend/Examples/BasicToDo/Main.hs
+++ frontend/src/Frontend/Examples/BasicToDo/Main.hs
@@ -23,18 +23,25 @@ app :: (DomBuilder t m, PostBuild t m, MonadFix m, MonadHold t m) => m ()
 app = elAttr "div" ("id" =: "todo-background" <>
                 "onclick" =: "document.getElementById('todo-input').focus()") $ do
         rec
-          let enter = keypress Enter input
-
-              modAttrsEv = getModAttrs <$> (True <$ enter)
+          let enter         = keypress Enter input
+              ev1           = leftmost [ True <$ enter
+                               , False <$ clickEv
+                               ]
+              modAttrsEv    = getModAttrs <$> ev1
               getModAttrs :: B.Bool -> M.Map AttributeName (Maybe T.Text)
               getModAttrs b = "style" =: Just ("visibility: " <> B.bool "visible" "hidden" b)
+          input <- inputElement $
+            def
+              & inputElementConfig_setValue .~ fmap (const "") enter
+              & inputElementConfig_elementConfig . elementConfig_initialAttributes .~
+                  ("id" =: "todo-input"
+                  <> "placeholder" =: "define hunter task")
+              & inputElementConfig_elementConfig . elementConfig_modifyAttributes .~ modAttrsEv
 
-          input <- inputElement $ def
-            & inputElementConfig_elementConfig . elementConfig_initialAttributes .~
-              ("id" =: "todo-input"
-               <> "placeholder" =: "define hunter task")
-            & inputElementConfig_elementConfig . elementConfig_modifyAttributes .~ modAttrsEv
-
-          let taskEv = tagPromptlyDyn (_inputElement_value input) enter
-          divClass "task" $ dynText =<< holdDyn "" taskEv
+          let taskEv  = tagPromptlyDyn (_inputElement_value input) enter
+              clickEv = domEvent Click el
+              ev      = leftmost [ taskEv
+                                 , "" <$ clickEv
+                                 ]
+          (el, _) <- elAttr' "div" ("class" =: "task") $ dynText =<< holdDyn "" ev
         pure()
